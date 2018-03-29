97ef65d8602f14f38a99cf2b9c1f1e503488f164
herobrinewu <31944560+herobrinewu@users.noreply.github.com>
2018-03-30 00:37:11 +0800

    Create CMM08.cpp

---
+++ Math_1/CMM08.cpp
@@ -0,0 +1,16 @@
+/*
+  2018 3 30:00:36 <create>
+  CMM08 簡單
+  計算兩數和的平方與立方值
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a,b;
+	cin >> a >> b;
+	cout << (a + b)*(a + b) << endl;
+	return 0;
+}
