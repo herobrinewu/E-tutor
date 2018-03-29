5bfb48ada5bdf6258c9d8267196689f248d2378f
herobrinewu <31944560+herobrinewu@users.noreply.github.com>
2018-03-30 00:35:43 +0800

    Create CMM07.cpp

---
+++ Math_1/CMM07.cpp
@@ -0,0 +1,16 @@
+/*
+  2018 3 30:00:35 <create>
+  CMM07 簡單
+  計算平方與立方值
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	cout << num << " " << num*num << " " << num*num*num << endl;
+	return 0;
+}
